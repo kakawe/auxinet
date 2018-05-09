6d24095a5f519982b86a8e7afb606e8a709517a2
adrianmg89 <a.marcos.g989@gmail.com>
2018-05-09 17:51:47 +0200

    principal

--- app/src/main/java/com/vpfc18/vpfc18/Principal/Perfil/Perfil_Fragment_1.java
+++ app/src/main/java/com/vpfc18/vpfc18/Principal/Perfil/Perfil_Fragment_1.java
@@ -21,7 +21,6 @@ public class Perfil_Fragment_1 extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
